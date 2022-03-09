c31ff0ae6260421545ee91af4d2fc4160a945de4
wendelroberta <wendel.roberta1995@gmail.com>
2022-03-08 23:15:04 -0300

    Concluindo os fundamentos de js

---
+++ expressoes-funcoes.js
@@ -0,0 +1,25 @@
+// function minhaFuncao(param){
+//     // bloco de codigo
+// }
+
+// minhaFuncao('param');
+
+// expressoes de função
+
+// const soma = function(num1, num2) { return num1 + num2 }
+// console.log(soma(1, 1));
+
+// diferenças principais
+
+console.log(apresentar())
+
+function apresentar(){
+    return "ola!";
+}
+
+// esse modo de declarar funcão n funciona se chamar a função antes
+const soma = function(num1, num2){
+    return num1 + num2
+}
+console.log(soma(1, 1))
+
---
+++ parametros.js
@@ -0,0 +1,25 @@
+// parâmetros de função
+
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+console.log(soma(2, 2));
+console.log(soma(245, 20));
+console.log(soma(-500, 60));
+
+// parâmetros X argumentos
+
+// ordem dos parâmetros
+
+function nomeIdade(nome, idade){
+    return `meu nome é ${nome} e minha idade é ${idade}`
+}
+
+// console.log(nomeIdade('Wendel', 26));
+
+function multiplica(numero1, numero2){
+    return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4,5), soma(3, 3)));
